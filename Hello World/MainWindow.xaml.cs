d29aa21f561ba0abc64d4c51dc009fd54f6b0d83
Jessica Earley-Cha <jearleycha@microsoft.com>
2025-01-22 11:02:12 -0800

    add SetMinWindowSize to make sure window is large enough for content

--- Hello World/MainWindow.xaml.cs
+++ Hello World/MainWindow.xaml.cs
@@ -10,7 +10,11 @@ using System.Diagnostics;
 using System.Text.RegularExpressions;
 using Hello_World.AIModel;
 using System.Threading.Tasks;
-
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
 
 namespace Hello_World
 {
@@ -19,7 +23,20 @@ namespace Hello_World
         public MainWindow()
         {
             this.InitializeComponent();
+            SetMinWindowSize(1100, 1100);  // Set the minimum width and height
             MainFrame.Navigate(typeof(MainPage));
+        }
+
+        private void SetMinWindowSize(int minWidth, int minHeight)
+        {
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            var windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            var appWindow = AppWindow.GetFromWindowId(windowId);
+
+            if (appWindow != null)
+            {
+                appWindow.Resize(new SizeInt32(minWidth, minHeight));
+            }
         }
     }
 }
